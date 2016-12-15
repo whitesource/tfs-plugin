3bad3fb018453acb302f70bef3c308680fffb117
Ruslan Gogerman <ruslangox@gmail.com>
2016-12-15 10:30:59 +0200

    fixed "fail on build" not uploading when OK. changed to path.sep for "file separator"

--- whitesource/WssTask.js
+++ whitesource/WssTask.js
@@ -20,6 +20,7 @@ var extensionsToHash = tl.getDelimitedInput('extensions', ' ', true);
 
 // Mandatory Fields
 var type = tl.getInput('type', true);
+if(type == "CHECK_POLICY_COMPLIANCE"){}
 var projectName = tl.getInput('projectName', true);
 
 // Optional Fields
@@ -56,18 +57,148 @@ if (notPermittedFolders.length != 0) {
 
 //Update the diff object
 directoryList.forEach(function (file) {
-    var fullPath = file.path + '\\' + file.name;
+    var fullPath = file.path + path.sep + file.name;
     if (isFile(fullPath) && isExtensionRight(file.name)) {
         var lastModified = getLastModifiedDate(fullPath);
         addToDependencies(file.name, fullPath, lastModified);
     }
 });
+function createPostRequest(){
+    var post_data = querystring.stringify({
+        "agent": "tfs-plugin",
+        "agentVersion": "1.0",
+        "type": type,
+        "token": auth.parameters.apitoken,
+        "timeStamp": new Date().getTime(),
+        "product": product,
+        "productVersion": productVersion,
+        "requesterEmail": requesterEmail,
+        "projectToken": projectToken,
+        "forceCheckAllDependencies": forceCheckAllDependencies
+    });
+    return post_data;
+}
+if(type == "CHECK_POLICY_COMPLIANCE") {
+// Build the post string from an object
+//     var post_data = querystring.stringify({
+//         "agent": "tfs-plugin",
+//         "agentVersion": "1.0",
+//         "type": type,
+//         "token": auth.parameters.apitoken,
+//         "timeStamp": new Date().getTime(),
+//         "product": product,
+//         "productVersion": productVersion,
+//         "requesterEmail": requesterEmail,
+//         "projectToken": projectToken,
+//         "forceCheckAllDependencies": forceCheckAllDependencies
+//     });
+     var post_data = createPostRequest() + "&diff=" + JSON.stringify(diff);
+
+
+// +------------+
+// | Sync POST |
+// +------------+
+    console.log('Sending data to Whitesource server');
+    var syncRes = syncRequest('POST', hostUrl, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Charset': 'utf-8'
+        },
+        body: post_data,
+        timeout: 600000
+    });
+    var totalFiles = JSON.stringify(diff[0].dependencies.length);
+    console.log('Total files was scanned: ' + totalFiles);
+
+
+    console.log('Checking for rejections');
+    var responseBody = syncRes.getBody('utf8');
+    if (isJson(responseBody)) {
+        var parsedRes = JSON.parse(syncRes.getBody('utf8'));
+        if (parsedRes.status == 2) { //STATUS_BAD_REQUEST
+            logError('Server responded with status "Bad Request", Please check your credentials');
+            logError('Terminating Build');
+            process.exit(1);
+        }
+        else if (parsedRes.status == 3) { //STATUS_SERVER_ERROR
+            logError('Server responded with status "Server Error", please try again');
+            logError('Terminating Build');
+            process.exit(1);
+        }
+
+    }
+    else {
+        logError('Server responded with object other than "JSON"');
+        logError(responseBody);
+        logError('Terminating Build');
+        process.exit(1);
+    }
+
+    var rejectionList = [];
+
+    var createRejectionList = function (currentNode) {
+        // the node is a leaf
+        if (typeof currentNode == 'string') {
+            return;
+        }
+        // the node is children array
+        else if (Array.isArray(currentNode) && currentNode.length != 0) {
+            currentNode.forEach(function (child) {
+                createRejectionList(child);
+            })
+        } else {
+            // The node if object or empty array
+            var objKeys = Object.keys(currentNode);
+            if (objKeys.length != 0) {
+                // The node is non empty object
+                var policyIndex = objKeys.indexOf('policy');
+                // There is a rejected policy
+                if (policyIndex != -1 && currentNode.policy.actionType == 'Reject') {
+                    var file_name = currentNode.resource.displayName;
+                    rejectionList.push(file_name);
+                } else {
+                    // Keep on going
+                    objKeys.forEach(function (key) {
+                        createRejectionList(currentNode[key]);
+                    })
+                }
+            } else {
+                // Empty object or array
+                return;
+            }
+        }
+    };
+
+    var resData = JSON.parse(parsedRes.data);
+    createRejectionList(resData);
+    var rejectionNum = rejectionList.length;
+
+
+// +--------------------+
+// | Result From Server |
+// +--------------------+
+
+
+    if (rejectionNum != 0) {
+        logError(rejectionNum + ' policy rejections');
+        logError('Files: ');
+        rejectionList.forEach(function (rejection) {
+            logError(rejection);
+        });
+        logError('Terminating Build');
+        process.exit(1);
+    }
+    else {
+        //creatResultOutput(resData);
+        console.log('No policy rejections found');
+    }
+}
 
 // Build the post string from an object
 var post_data = querystring.stringify({
     "agent": "tfs-plugin",
     "agentVersion": "1.0",
-    "type": type,
+    "type": "UPDATE",
     "token": auth.parameters.apitoken,
     "timeStamp": new Date().getTime(),
     "product": product,
@@ -161,10 +292,85 @@ var rejectionNum = rejectionList.length;
 // +--------------------+
 // | Result From Server |
 // +--------------------+
-var wssResult = __dirname + '\\realResult.md';
+
+
 if (rejectionNum != 0) {
-    var totalProjects = resData.projects;
-    var firstArray = totalProjects[0];
+    logError(rejectionNum + ' policy rejections');
+    logError('Files: ');
+    rejectionList.forEach(function (rejection) {
+        logError(rejection);
+    });
+    logError('Terminating Build');
+    process.exit(1);
+}
+else {
+    creatResultOutput(resData);
+    console.log('No policy rejections found');
+}
+
+
+
+// var wssResult = __dirname + '\\realResult.md';
+// if (rejectionNum != 0) {
+//     var totalProjects = resData.projects;
+//     var firstArray = totalProjects[0];
+// //WRITING RESULT FILE
+//     if (totalProjects[0] != null) {
+//         var projectName = totalProjects[0].name;
+//         var projectUrl = totalProjects[0].url;
+//         var projectNew = totalProjects[0].newlyCreated;
+//         var projectAlerts = totalProjects[0].totalAlerts;
+//         var projectVulnerable = totalProjects[0].totalVulnerableLibraries;
+//         var projectPolicy = totalProjects[0].totalPolicyViolations;
+// // ToDo: maybe add server time instead of client time
+//         var formatted = moment(totalProjects[0].date, 'MMM D, YYYY h:mm:ss A').format('dddd, MMMM D, YYYY h:mm A');
+//         var projectDate = formatted;
+//     }
+// // var mdFile = '<div style="padding:5px 0px"> <span>Vulnerabilities Summary:</span> </div> <table border="0" style="border-top: 1px solid #eee;border-collapse: separate;border-spacing: 0 2px;">Project name:' + projectName + ' <br>Project url:' + projectUrl + ' <br>Project new:' + projectNew + ' <br>Project totalAlerts:' + projectAlerts + ' <br>Project totalVulnerableLibraries:' + projectVulnerable + ' <br>Project totalPolicyViolations:' + projectPolicy + ' <br>Project date:' + projectDate + ' <br> <table> <tr> <td>Project name</td> <td>' + projectName + '</td> </tr> <tr> <td>Project url:</td> <td>' + projectUrl + '</td> </tr> <tr>\<td>Project new</td> <td>' + projectNew + '</td> </tr> </table> <a target="_blank" href="https://saas.whitesourcesoftware.com/Wss/WSS.html">For more Information</a> </div>';
+//     var mdFile = '<div style="padding:5px 0px"> <span>Vulnerabilities Summary:</span> </div> <table border="0" style="border-top: 1px solid #eee;border-collapse: separate;border-spacing: 0 2px;"> <table> <tr> <td>Project name</td> <td></td> <td>' + projectName + '</td> </tr> <tr> <td>Project url:</td> <td></td> <td>' + projectUrl + '</td> </tr> <tr> <td>Project new</td> <td></td> <td>' + projectNew + '</td> </tr> <tr> <td>Project totalAlerts</td> <td></td> <td>' + projectAlerts + '</td> </tr> <tr> <td>Project totalVulnerableLibraries</td> <td></td> <td>' + projectVulnerable + '</td> </tr> <tr> <td>Project totalPolicyViolations</td> <td></td> <td>' + projectPolicy + '</td> </tr> <tr> <td>Project date</td> <td></td> <td>' + projectDate + '</td> </tr> </table> <a target="_blank" href="https://saas.whitesourcesoftware.com/Wss/WSS.html">For more Information</a> </div>';
+//     fs.writeFile(wssResult, mdFile, function (err) {
+//         if (err)
+//             return console.log(err);
+//     });
+// }
+// else {
+//     var mdFile = '<div style="padding:5px 0px"> <H2><span>Policy rejection occurred </span></H2></div>';
+//     fs.writeFile(wssResult, mdFile, function (err) {
+//         if (err)
+//             return console.log(err);
+//     });
+// }
+
+//var wssResult = __dirname + '\\result.md';
+
+// console.log("##vso[task.addattachment type=Distributedtask.Core.Summary;name=Wss Report;]" + wssResult);
+
+// +-------------------+
+// | Build Termination |
+// +-------------------+
+
+// if (rejectionNum != 0) {
+//     logError(rejectionNum + ' policy rejections');
+//     logError('Files: ');
+//     rejectionList.forEach(function (rejection) {
+//         logError(rejection);
+//     });
+//     logError('Terminating Build');
+//     process.exit(1);
+// } else {
+//     console.log('No policy rejections found');
+// }
+
+
+// +------------------+
+// | Define functions |
+// +------------------+
+
+function creatResultOutput(responseData) {
+    var wssResult = __dirname + path.sep+ 'realResult.md';
+    console.log(responseData);
+    var totalProjects = responseData.projects;
+    console.log(totalProjects);
 //WRITING RESULT FILE
     if (totalProjects[0] != null) {
         var projectName = totalProjects[0].name;
@@ -177,45 +383,13 @@ if (rejectionNum != 0) {
         var formatted = moment(totalProjects[0].date, 'MMM D, YYYY h:mm:ss A').format('dddd, MMMM D, YYYY h:mm A');
         var projectDate = formatted;
     }
-// var mdFile = '<div style="padding:5px 0px"> <span>Vulnerabilities Summary:</span> </div> <table border="0" style="border-top: 1px solid #eee;border-collapse: separate;border-spacing: 0 2px;">Project name:' + projectName + ' <br>Project url:' + projectUrl + ' <br>Project new:' + projectNew + ' <br>Project totalAlerts:' + projectAlerts + ' <br>Project totalVulnerableLibraries:' + projectVulnerable + ' <br>Project totalPolicyViolations:' + projectPolicy + ' <br>Project date:' + projectDate + ' <br> <table> <tr> <td>Project name</td> <td>' + projectName + '</td> </tr> <tr> <td>Project url:</td> <td>' + projectUrl + '</td> </tr> <tr>\<td>Project new</td> <td>' + projectNew + '</td> </tr> </table> <a target="_blank" href="https://saas.whitesourcesoftware.com/Wss/WSS.html">For more Information</a> </div>';
     var mdFile = '<div style="padding:5px 0px"> <span>Vulnerabilities Summary:</span> </div> <table border="0" style="border-top: 1px solid #eee;border-collapse: separate;border-spacing: 0 2px;"> <table> <tr> <td>Project name</td> <td></td> <td>' + projectName + '</td> </tr> <tr> <td>Project url:</td> <td></td> <td>' + projectUrl + '</td> </tr> <tr> <td>Project new</td> <td></td> <td>' + projectNew + '</td> </tr> <tr> <td>Project totalAlerts</td> <td></td> <td>' + projectAlerts + '</td> </tr> <tr> <td>Project totalVulnerableLibraries</td> <td></td> <td>' + projectVulnerable + '</td> </tr> <tr> <td>Project totalPolicyViolations</td> <td></td> <td>' + projectPolicy + '</td> </tr> <tr> <td>Project date</td> <td></td> <td>' + projectDate + '</td> </tr> </table> <a target="_blank" href="https://saas.whitesourcesoftware.com/Wss/WSS.html">For more Information</a> </div>';
     fs.writeFile(wssResult, mdFile, function (err) {
         if (err)
             return console.log(err);
     });
+    console.log("##vso[task.addattachment type=Distributedtask.Core.Summary;name=Wss Report;]" + wssResult);
 }
-else{
-    var mdFile ='<div style="padding:5px 0px"> <H2><span>Policy rejection occurred </span></H2></div>';
-    fs.writeFile(wssResult, mdFile, function (err) {
-        if (err)
-            return console.log(err);
-    });
-}
-
-//var wssResult = __dirname + '\\result.md';
-
-console.log("##vso[task.addattachment type=Distributedtask.Core.Summary;name=Wss Report;]" + wssResult);
-
-// +-------------------+
-// | Build Termination |
-// +-------------------+
-
-if (rejectionNum != 0) {
-    logError(rejectionNum + ' policy rejections');
-    logError('Files: ');
-    rejectionList.forEach(function (rejection) {
-        logError(rejection);
-    });
-    logError('Terminating Build');
-    process.exit(1);
-} else {
-    console.log('No policy rejections found');
-}
-
-
-// +------------------+
-// | Define functions |
-// +------------------+
 
 function readDirRecursive(dir, fileList) {
     var notPermittedFolders = [];
@@ -233,15 +407,15 @@ function readDirRecursive(dir, fileList) {
     for (var i = 0; i < files.length; i++) {
         var file = files[i];
         try {
-            fs.statSync(dir + '\\' + file).isDirectory();
+            fs.statSync(dir + path.sep + file).isDirectory();
         }
         catch (err) {
             logError(err);
             notPermittedFolders.push(file);
             continue;
         }
-        if (fs.statSync(dir + '\\' + file).isDirectory()) {
-            readDirObject = readDirRecursive(dir + '\\' + file, fileList);
+        if (fs.statSync(dir + path.sep + file).isDirectory()) {
+            readDirObject = readDirRecursive(dir + path.sep + file, fileList);
             fileList = readDirObject.fileList;
         } else {
             fileList.push({'name': file, 'path': dir});
